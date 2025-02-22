dec971069b322678c911df9bd465ee7d03401017
Saravanan R <saravanan.r2394@gmail.com>
2025-02-22 15:38:21 +0530

    'all'

--- test_script.py
+++ test_script.py
@@ -4,10 +4,10 @@ from selenium import webdriver
 driver = webdriver.Chrome()  # Or use webdriver.Firefox()
 
 # Open a website
-driver.get("https://www.wikipedia.com")
+driver.get("https://www.google.com")
 
 # Verify the title
-assert "Wikipedia" in driver.title
+assert "Google" in driver.title
 
 # Close the browser
 driver.quit()
